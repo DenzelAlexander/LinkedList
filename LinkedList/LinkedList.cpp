2a77820194a2992246fbce763ff7e7f0aa4d6561
DenzelAlexander <denzel.a.alexander@gmail.com>
2021-06-09 22:05:52 -0700

    Add project files.

---
+++ LinkedList/LinkedList.cpp
@@ -0,0 +1,29 @@
+
+
+#include <iostream>
+
+int main()
+{
+   
+    std::cout << "Hello World!\n";
+}
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void insert(int newdata) {
+    Node * newNode= new Node;
+    newNode-> data = newdata;
+
+}
+
+void remove() {
+
+}
+
+void print() {
+
+}
+
+
